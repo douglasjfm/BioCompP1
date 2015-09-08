55c2c1c195bb0ce682ad6bd1f3d08534a74075ca
douglasjfm <douglasjfm@gmail.com>
2015-09-07 21:15:11 -0300

    alinhamento completo via console

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include <gmodule.h>
+#include <string.h>
+
+#define UI_FILE "ui.glade"
+
+void alinhar (char *s, char *t)
+{
+    return;
+}
+
+G_MODULE_EXPORT void cb_alinhar (GtkWidget *btn, gpointer data)
+{
+    char cad1[101], cad2[101];
+    GtkBuilder *builder = GTK_BUILDER(data);
+    GtkEntry *uicad1 = NULL, *uicad2 = NULL;
+
+    uicad1 = GTK_ENTRY(gtk_builder_get_object(builder,"uiCadeia1"));
+    uicad2 = GTK_ENTRY(gtk_builder_get_object(builder,"uiCadeia2"));
+
+    strcpy(cad1,uicad1->text);
+    strcpy(cad2,uicad2->text);
+
+    if (!p1_valida_cadeia(cad1) || !p1_valida_cadeia(cad2))
+    {
+        g_print("Entrada(s) Invalidas.");
+    }
+    else
+    {
+        p1_alinhar_s_t(cad1,cad2);
+    }
+
+    g_free(uicad1);
+    g_free(uicad2);
+}
+
+int runGTK (int argc, char *argv[])
+{
+    GtkBuilder *builder = NULL;
+    GError *error = NULL;
+    GtkWidget *win = NULL;
+
+    /* Initialize GTK+ */
+    gtk_init (&argc, &argv);
+
+    builder = gtk_builder_new();
+
+    if (!gtk_builder_add_from_file(builder,UI_FILE,&error))
+    {
+        g_warning("P1 Erro: %s",error->message);
+        g_free(error);
+        return 0xE;
+    }
+
+    win = GTK_WIDGET(gtk_builder_get_object(builder,"janela"));
+    gtk_builder_connect_signals(builder, builder);
+    g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+
+    /* Enter the main loop */
+    gtk_widget_show_all (win);
+    gtk_main ();
+    return 0;
+}
+
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+        return runGTK(argc, argv);
+    if (argc > 2)
+    {
+        printf("P1 Console\n");
+        if (p1_valida_cadeia(argv[1]) && p1_valida_cadeia(argv[2]))
+        {
+            printf("Cadeias OK!\n");
+            p1_alinhar_s_t(argv[1],argv[2]);
+        }
+    }
+    return 0;
+}
---
+++ p1.c
@@ -0,0 +1,192 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/*Alfabeto*/
+#define GAP '-'
+#define NUC_A 'a'
+#define NUC_G 'g'
+#define NUC_C 'c'
+#define NUC_T 't'
+#define NUC_U 'u'
+
+/*Pontuações*/
+#define SCR_M  5
+#define SCR_S -1
+#define SCR_R -6
+#define SCR_I -8
+
+/*Dierções*/
+#define DIRSUB 0x01
+#define DIRESQ 0x02
+#define DIRDIA 0x04
+
+#define goUP(x) (x & DIRSUB)
+#define goLF(x) (x & DIRESQ)
+#define goDG(x) (x & DIRDIA)
+
+/*max3: o valor maximo de uma tripla x, y e z*/
+#define max3(x,y,z) ((x > y) && (x > z)) ? x : ((y > z) ? y : z)
+
+int p1_valida_cadeia (char *c)
+{
+    if (c)
+    {
+        int l = strlen(c);
+        int i;
+        for (i = 0; i < l; i++)
+        {
+            char nuc = c[i];
+            if (nuc != 'a' && nuc != 'c' && nuc != 'g' && nuc != 't' && nuc != 'u')
+                return 0;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+char** alloccharmtx(int i,int j)
+{
+    char **ret;
+    ret = (char **) calloc (i, sizeof(char*));
+    int c;
+    for (c = 0; c < i; c++)
+        ret[c] = (char *) calloc(j, sizeof(char));
+
+    return ret;
+}
+
+void freemtx(char **m, int i)
+{
+    int c;
+    for (c = 0; c < i; c++)
+        free(m[c]);
+    free(m);
+}
+
+/*!
+    Função para encontrar o melhor alinhamento entre as cadeias s e t,
+    dado que s e t sejam cadeias validas de nucleotideos (a,c,g,t).
+
+    @param s ponteiro para a cadeia s
+    @param t ponteiro para a cadeia t, a ser alinhada com s
+    @return TODO
+*/
+void p1_alinhar_s_t (char *s, char *t)
+{
+    int slen, tlen, lignlen;
+    int i, j, k, l, maxij, diagv, esqv, cimav;
+    char dir;
+    char **m, **mdir;
+    char *slign, *tlign;
+    int mrows, mcols;
+
+    /*Alocação e  inicialização da matriz de scores*/
+    slen = strlen(s);
+    tlen = strlen(t);
+
+    mrows = tlen + 1;
+    mcols = slen + 1;
+
+    m = alloccharmtx(mrows,mcols);
+    mdir = alloccharmtx(mrows,mcols);
+
+    for (i = 1; i < mcols; i++)
+    {
+        m[0][i] = i * SCR_R;
+        mdir[0][i] = DIRESQ;
+    }
+
+    for (i = 1; i < mrows; i++)
+    {
+        m[i][0] = i * SCR_I;
+        mdir[i][0] = DIRSUB;
+    }
+
+    for (i = 1; i < mrows; i++)
+        for (j = 1; j < mcols; j++)
+        {
+            esqv = m[i][j-1] + SCR_I;
+            diagv = (s[j-1] == t[i-1]) ? (m[i-1][j-1] + SCR_M) : (m[i-1][j-1] + SCR_S);
+            cimav = m[i-1][j] + SCR_R;
+
+            maxij = max3(esqv,diagv,cimav);
+
+            dir = 0;
+
+            if (maxij == esqv)
+                dir = DIRESQ;
+            if (maxij == cimav)
+                dir = dir | DIRSUB;
+            if (maxij == diagv)
+                dir = dir | DIRDIA;
+
+            mdir[i][j] = dir;
+            m[i][j] = maxij;
+        }
+
+    /*Alocação das strings s e t alinhadas*/
+
+    lignlen = slen > tlen ? slen : tlen;
+
+    slign = (char *) calloc(lignlen + 1, sizeof(char));
+    tlign = (char *) calloc(lignlen + 1, sizeof(char));
+
+    i = mrows - 1;
+    j = mcols - 1;
+
+    slign[lignlen] = '\0';
+    tlign[lignlen] = '\0';
+
+    k = lignlen - 1;
+
+    do
+    {
+        dir = mdir[i][j];
+        if (goDG(dir))
+        {
+            slign[k] = s[j-1];
+            tlign[k] = t[i-1];
+            i--;
+            j--;
+        }
+        else if (goLF(dir))
+        {
+            slign[k] = s[j-1];
+            tlign[k] = GAP;
+            j--;
+        }
+        else if (goUP(dir))
+        {
+            slign[k] = GAP;
+            tlign[k] = t[i-1];
+            i--;
+        }
+
+        k--;
+
+    }while(dir);
+
+    printf("\n%s\n%s\n\n",slign,tlign);
+
+    for (i = 0; i < mrows; i++)
+    {
+        for (j = 0; j < mcols; j++)
+        {
+            printf("%03d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (i = 0; i < mrows; i++)
+    {
+        for (j = 0; j < mcols; j++)
+        {
+            printf("%03d ", mdir[i][j]);
+        }
+        printf("\n");
+    }
+
+    freemtx(m,mrows);
+    freemtx(mdir,mrows);
+}
